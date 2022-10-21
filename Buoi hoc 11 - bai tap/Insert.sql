69d99d105df9633761f36de151be8f444444bbf9
ductrongT2207E <trongdd232@gmail.com>
2022-10-21 17:53:07 +0700

    Update Insert.sql

--- Buoi hoc 11 - bai tap/Insert.sql
+++ Buoi hoc 11 - bai tap/Insert.sql
@@ -72,3 +72,34 @@ select * from MUCPHI;
 
  select * from DANGKYCUNGCAP;
  drop table DANGKYCUNGCAP;
+
+--Câu 3: Liệt kê những dòng xe có số chỗ ngồi trên 5 chỗ
+select * from DONGXE where SoChoNgoi > 5;
+
+--Câu 4: Liệt kê thông tin của các nhà cung cấp đã từng đăng ký cung cấp những dòng xe thuộc hãng xe “Toyota” 
+--với mức phí có đơn giá là 15.000 VNĐ/km hoặc những dòng xe thuộc hãng xe “KIA” với mức phí có đơn giá là 20.000 VNĐ/km 
+select * from NHACUNGCAP where MaNhaCC in 
+(select MaNhaCC from DANGKYCUNGCAP where MaMP in
+(select MaMP from MUCPHI where MaMP like 'MP02'));
+
+--Câu 5: Liệt kê thông tin toàn bộ nhà cung cấp được sắp xếp tăng dần theo tên nhà cung cấp và giảm dần theo mã số thuế 
+select * from NHACUNGCAP order by TenNhaCC asc;
+select * from NHACUNGCAP order by MaSoThue desc;
+ 
+--Câu 6: Đếm số lần đăng ký cung cấp phương tiện tương ứng cho từng nhà cung cấp với yêu cầu chỉ đếm cho những nhà cung 
+--cấp thực hiện đăng ký cung cấp có ngày bắt đầu cung cấp là “20/11/2015” 
+
+
+--Câu 7: Liệt kê tên của toàn bộ các hãng xe có trong cơ sở dữ liệu với yêu cầu mỗi hãng xe chỉ được liệt kê một lần
+
+ 
+--Câu 8: Liệt kê MaDKCC, MaNhaCC, TenNhaCC, DiaChi, MaSoThue, TenLoaiDV, DonGia, HangXe, NgayBatDauCC, NgayKetThucCC 
+--của tất cả các lần đăng ký cung cấp phương tiện với yêu cầu những nhà cung cấp nào chưa từng thực hiện đăng ký cung 
+--cấp phương tiện thì cũng liệt kê thông tin những nhà cung cấp đó ra 
+
+
+--Câu 9: Liệt kê thông tin của các nhà cung cấp đã từng đăng ký cung cấp phương tiện thuộc dòng xe “Hiace” hoặc từng 
+--đăng ký cung cấp phương tiện thuộc dòng xe “Cerato” 
+
+
+--Câu 10: Liệt kê thông tin của các nhà cung cấp chưa từng thực hiện đăng ký cung cấp phương tiện lần nào cả.
